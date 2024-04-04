d2d425c7fa965158dc653ab1b4ba1287efbd3bcf
Oisisushi <osushi.oisisushi@gmail.com>
2024-04-05 08:13:46 +0900

    レッスン34 チャプター7 日報情報管理画面の実装 コントローラを作成
    日報一覧画面の表示メソッドのみ実装

---
+++ src/main/java/com/techacademy/controller/ReportController.java
@@ -0,0 +1,30 @@
+package com.techacademy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.techacademy.constants.ErrorKinds;
+import com.techacademy.constants.ErrorMessage;
+
+
+@Controller
+@RequestMapping("reports")
+public class ReportController {
+
+    // 日報一覧画面
+    @GetMapping
+    public String list() {
+        return "reports/list";
+    }
+
+}
