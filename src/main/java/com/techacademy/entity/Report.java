dd932b6d677e302f8650825518b095618a943838
Oisisushi <osushi.oisisushi@gmail.com>
2024-04-06 13:43:07 +0900

    エラーが発生するバージョン(JoinColumnの記述がスネークケース)
    Table [reports] contains physical column name [employee_code] referred
    to by multiple logical column names: [employee_code], [employeeCode]

--- src/main/java/com/techacademy/entity/Report.java
+++ src/main/java/com/techacademy/entity/Report.java
@@ -4,11 +4,17 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
@@ -18,32 +24,47 @@ import lombok.Data;
 public class Report {
 
     // ID
-    private String code;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotEmpty
+    private Integer ID;
 
     // 日付
+    @Column(nullable = false)
     private LocalDate reportDate;
 
     // タイトル
+    @Column(length = 100, nullable = false)
+    @NotEmpty
+    @Length(max = 100)
     private String title;
 
     // 内容
+    @Column(columnDefinition="LONGTEXT", nullable = false)
+    @NotEmpty
     private String content;
 
     // 社員番号
+    @Column(length = 10, nullable = false)
+    @NotEmpty
+    @Length(max = 10)
     private String employeeCode;
 
     // 削除フラグ
+    @Column(columnDefinition="TINYINT", nullable = false)
     private boolean deleteFlg;
 
     // 登録日時
-    private LocalDateTime criatedAt;
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
 
     // 更新日時
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
 
 
-//    @ManyToOne
-//    @JoinColumn(name = "employee_code", referencedColumnName = "code", nullable = false)
-//    private Employee employee;
+    @ManyToOne
+    @JoinColumn(name = "employee_code", referencedColumnName = "code", nullable = false)
+    private Employee employee;
 
 }
