5d3ac6a19965d6b16204d34f90262bdbe17fbe75
Oisisushi <osushi.oisisushi@gmail.com>
2024-04-06 10:46:22 +0900

    テーブル定義書とエンティティクラスEmployeeを参考にしながらフィールドを定義

--- src/main/java/com/techacademy/entity/Report.java
+++ src/main/java/com/techacademy/entity/Report.java
@@ -1,17 +1,14 @@
 package com.techacademy.entity;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import org.hibernate.annotations.SQLRestriction;
-import org.hibernate.validator.constraints.Length;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
@@ -19,5 +16,34 @@ import lombok.Data;
 @Table(name="reports")
 @SQLRestriction("delete_flg = false")
 public class Report {
+
+    // ID
+    private String code;
+
+    // 日付
+    private LocalDate reportDate;
+
+    // タイトル
+    private String title;
+
+    // 内容
+    private String content;
+
+    // 社員番号
+    private String employeeCode;
+
+    // 削除フラグ
+    private boolean deleteFlg;
+
+    // 登録日時
+    private LocalDateTime criatedAt;
+
+    // 更新日時
+    private LocalDateTime updatedAt;
+
+
+//    @ManyToOne
+//    @JoinColumn(name = "employee_code", referencedColumnName = "code", nullable = false)
+//    private Employee employee;
 
 }
