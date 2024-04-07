d5e2aa2f1c632f019888424f24d2866a758cfa24
Oisisushi <osushi.oisisushi@gmail.com>
2024-04-08 07:54:12 +0900

    日報一覧画面表示メソッドを修正
    ユーザーが管理者ではない場合はそのユーザーの日報のみを表示するように変更、同時にサービスとリポジトリも修正

--- src/main/java/com/techacademy/controller/ReportController.java
+++ src/main/java/com/techacademy/controller/ReportController.java
@@ -15,15 +15,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.techacademy.constants.ErrorKinds;
 import com.techacademy.constants.ErrorMessage;
+import com.techacademy.entity.Employee;
+import com.techacademy.service.ReportService;
+import com.techacademy.service.UserDetail;
 
 
 @Controller
 @RequestMapping("reports")
 public class ReportController {
 
+    private final ReportService reportService;
+
+    @Autowired
+    public ReportController (ReportService reportService) {
+        this.reportService = reportService;
+    }
+
     // 日報一覧画面
     @GetMapping
-    public String list() {
+    public String list(@AuthenticationPrincipal UserDetail userDetail, Model model) {
+
+        // ログインユーザーの権限情報を取得
+        Employee.Role role = userDetail.getEmployee().getRole();
+
+        //　ログインユーザーによって検索内容を変更
+        // ユーザーがADMINの場合
+        if(Employee.Role.ADMIN.equals(role)) {
+
+            model.addAttribute("listSize", reportService.findAll().size());
+            model.addAttribute("reportList", reportService.findAll());
+
+        // ユーザーがADMINではない場合
+        } else {
+            String employeeCode = userDetail.getEmployee().getCode();
+            model.addAttribute("reportList", reportService.findByEmployeeCode(employeeCode));
+            model.addAttribute("listSize", reportService.findByEmployeeCode(employeeCode).size());
+        }
+
         return "reports/list";
     }
 
