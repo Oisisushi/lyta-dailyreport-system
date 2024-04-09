3f22001dfd5db060b69dd3bd7cfa7e46f6ddbc33
Oisisushi <osushi.oisisushi@gmail.com>
2024-04-09 16:52:54 +0900

    employee_codeとreport_dateが両方一致する日報があるかをチェックするメソッドを追加

--- src/main/java/com/techacademy/repository/ReportRepository.java
+++ src/main/java/com/techacademy/repository/ReportRepository.java
@@ -1,5 +1,6 @@
 package com.techacademy.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,5 +13,8 @@ public interface ReportRepository extends JpaRepository<Report, String> {
 
     // employee_codeが一致する日報を検索してリスト化する
     List<Report> findByEmployeeCode(String employeeCode);
+
+    // employee_codeとreport_dateが一致する日報を検索する
+    boolean existsByEmployeeCodeAndReportDate(String employeeCode, LocalDate reportDate);
 
 }
