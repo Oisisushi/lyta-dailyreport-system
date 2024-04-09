6bba70f2afe018baeff5f8cdec49beedba6d1189
Oisisushi <osushi.oisisushi@gmail.com>
2024-04-09 17:05:04 +0900

    日報保存処理を修正、日報新規登録画面の業務チェック用にメソッドを追加
    業務チェック「ログイン中の従業員 かつ 入力した日付」の日報データが存在するかをチェックするメソッドを追加
    日報保存処理部分にメソッドを呼び出す記述を追加

--- src/main/java/com/techacademy/service/ReportService.java
+++ src/main/java/com/techacademy/service/ReportService.java
@@ -2,7 +2,6 @@ package com.techacademy.service;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -36,6 +35,12 @@ public class ReportService {
     // 日報保存処理
     public ErrorKinds save(Report report) {
 
+        // 日報重複チェック
+        ErrorKinds result = reportDateCheck(report);
+        if (ErrorKinds.CHECK_OK != result) {
+            return result;
+        }
+
         report.setDeleteFlg(false);
 
         LocalDateTime now = LocalDateTime.now();
@@ -45,6 +50,16 @@ public class ReportService {
         reportRepository.save(report);
 
         return ErrorKinds.SUCCESS;
+    }
+
+    // ログイン中の従業員 かつ 入力した日付 の日報データが存在するかをチェックする処理
+    private ErrorKinds reportDateCheck(Report report) {
+
+        if(reportRepository.existsByEmployeeCodeAndReportDate(report.getEmployeeCode(), report.getReportDate())) {
+            return ErrorKinds.DATECHECK_ERROR;
+        }
+
+        return ErrorKinds.CHECK_OK;
     }
 
 
